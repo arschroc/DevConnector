da024a5b17e12d2bcb00d5656fd506a5343edc84
Aaron Schrock <aaronschrock@Aarons-MBP.frontierlocal.net>
2019-02-06 17:31:16 -0800

    Add users

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-// @route   GET api/profile/users
+// @route   GET api/profile/test
 // @desc    Tests profile route
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
