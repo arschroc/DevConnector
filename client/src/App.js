f4ec4d3fe445f6df36ec65a4a79fd24415991b6d
arschroc <arschroc@gmail.com>
2019-02-26 15:51:42 -0800

    Start Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authAction";
+import { clearCurrentProfile } from "./actions/profileActions";
 import store from "./store";
 
 import Navbar from "./components/layout/Navbar";
@@ -11,6 +12,7 @@ import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Dashboard from "./components/dashboard/Dashboard";
 import "./App.css";
 
 //Check for token
@@ -28,7 +30,7 @@ if (localStorage.jwtToken) {
     //Logout user
     store.dispatch(logoutUser());
     //TODO Clear current profile
-
+    store.dispatch(clearCurrentProfile());
     //redirect to login
     window.location.href = "/login";
   }
@@ -45,6 +47,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>
