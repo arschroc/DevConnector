85d4f1c8d75c571e0201f5bfa840a636b8502597
arschroc <arschroc@gmail.com>
2019-03-08 11:08:56 -0800

    Add display of posts

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import NotFound from "./components/not-found/NotFound";
+import Posts from "./components/posts/Posts";
 
 //Check for token
 if (localStorage.jwtToken) {
@@ -74,18 +75,21 @@ class App extends Component {
                 />
               </Switch>
               <Switch>
-                <AddExperience
+                <PrivateRoute
                   exact
                   path="/add-experience"
                   component={AddExperience}
                 />
               </Switch>
               <Switch>
-                <AddEducation
+                <PrivateRoute
                   exact
                   path="/add-education"
                   component={AddEducation}
                 />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
               </Switch>
               <Route exact path="/profiles" component={Profiles} />
               <Route exact path="/profile/:handle" component={Profile} />
